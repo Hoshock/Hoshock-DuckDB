839015ea64c467b5d275ca1919d068cd1caa1eda
Tsuyoshi ISHIDA <ish-ts@wni.com>
2026-01-02 16:11:38 +0900

    feat: ✨ add initial flake.nix and .envrc configuration files

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter."${system}" = pkgs.nixfmt-tree;
+
+      devShells."${system}".default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          gcc
+        ];
+      };
+    };
+}
